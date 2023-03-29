7d6cf8914ed3083fa20d5da802576047528cbd9b
dangkiet2000 <dangvankiet.11c5@gmail.com>
2023-03-29 16:05:38 +0700

    fix warning ws client

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = "https://note-app-server-a745.onrender.com";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://note-app-server-a745.onrender.com/graphql";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "wss://note-app-server-a745.onrender.com/graphql";
